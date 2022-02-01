a6870c8828545bfb370f1e871a374a5740e8ec89
Mirjahon Ulmasov <mirjahonulmasov@MacBook-Pro-Mirjahon.local>
2022-02-01 09:44:56 +0500

    asd

---
+++ src/views/SuperAdmin/AdminCreate.vue
@@ -0,0 +1,385 @@
+<template>
+  <div>
+    <form @submit.prevent="submitHandler">
+      <div class="form-input">
+        <h4>Выберите роль</h4>
+        <v-select
+          v-model="user.role"
+          style="width: 375px"
+          :options="['admin']"
+          id="select-state"
+        >
+          <template #open-indicator="{ attributes }">
+            <span v-bind="attributes">
+              <img src="../../assets/images/icons/select-icon.svg" alt="" />
+            </span>
+          </template>
+        </v-select>
+      </div>
+      <div class="form-input">
+        <h4>Ф.И.О.</h4>
+        <my-input
+          type="input"
+          :width="375"
+          v-model="user.full_name"
+          :error="errors.has('full_name')"
+          name="full_name"
+          v-validate="'required|min:5'"
+        />
+        <span class="error-text" v-show="errors.has('full_name')">
+          <feather-icon
+            :icon="'InfoIcon'"
+            style="color: #db2379 !important; margin-right: 5px"
+            svgClasses="h-6 w-6"
+          />
+          {{ errors.first("full_name") }}
+        </span>
+      </div>
+      <div class="form-input">
+        <h4>Телефон номера</h4>
+        <span class="phone-code">+998</span>
+        <my-input
+          padding="14px 70px"
+          type="text"
+          :width="375"
+          v-model="user.phone_number"
+          :error="errors.has('phone')"
+          name="phone"
+          v-validate="'required|numeric|length:9'"
+        />
+        <span class="error-text" v-show="errors.has('phone')">
+          <feather-icon
+            :icon="'InfoIcon'"
+            style="color: #db2379 !important; margin-right: 5px"
+            svgClasses="h-6 w-6"
+          />
+          {{ errors.first("phone") }}
+        </span>
+      </div>
+      <div class="form-input">
+        <h4>Email</h4>
+        <my-input
+          type="email"
+          :width="375"
+          v-model="user.email"
+          :error="errors.has('required|email')"
+          name="email"
+          v-validate="'email'"
+        />
+        <span class="error-text" v-show="errors.has('email')">
+          <feather-icon
+            :icon="'InfoIcon'"
+            style="color: #db2379 !important; margin-right: 5px"
+            svgClasses="h-6 w-6"
+          />
+          {{ errors.first("email") }}
+        </span>
+      </div>
+      <div class="form-input">
+        <h4>Имя пользователя</h4>
+        <my-input
+          type="input"
+          :width="375"
+          v-model="user.username"
+          :error="errors.has('username')"
+          name="username"
+          v-validate="'required|min:4'"
+        />
+        <span class="error-text" v-show="errors.has('username')">
+          <feather-icon
+            :icon="'InfoIcon'"
+            style="color: #db2379 !important; margin-right: 5px"
+            svgClasses="h-6 w-6"
+          />
+          {{ errors.first("username") }}
+        </span>
+      </div>
+      <div class="form-input">
+        <h4>Пароль</h4>
+        <my-input
+          type="password"
+          name="password"
+          v-model="password"
+          ref="password"
+          :width="375"
+          :error="errors.has('password')"
+          v-validate="'min:5'"
+        />
+        <span class="error-text" v-show="errors.has('password')">
+          <feather-icon
+            :icon="'InfoIcon'"
+            style="color: #db2379 !important; margin-right: 5px"
+            svgClasses="h-6 w-6"
+          />
+          {{ errors.first("password") }}
+        </span>
+      </div>
+      <div class="form-input">
+        <h4>Потвердите пароль</h4>
+        <my-input
+          type="password"
+          :width="375"
+          v-model="confirm"
+          :error="errors.has('confirm')"
+          name="confirm"
+          data-vv-as="password"
+          v-validate="'min:5|confirmed:password'"
+        />
+        <span class="error-text" v-show="errors.has('confirm')">
+          <feather-icon
+            :icon="'InfoIcon'"
+            style="color: #db2379 !important; margin-right: 5px"
+            svgClasses="h-6 w-6"
+          />
+          {{ errors.first("confirm") }}
+        </span>
+      </div>
+      <div>
+        <h4 class="notify">Уведомления</h4>
+        <div class="form-group">
+          <input v-model="user.site_notifications" type="checkbox" id="news" />
+          <label for="news">Получать обновления</label>
+        </div>
+        <div class="form-group">
+          <input
+            v-model="user.email_notifications"
+            type="checkbox"
+            id="email"
+          />
+          <label for="email">Получать сообщения на почту</label>
+        </div>
+      </div>
+      <div class="actions">
+        <my-button
+          @click.native="handlerTwo()"
+          type="button"
+          title="Отменить"
+          bgColor="#EDF1FD"
+          color="#4679EC"
+        ></my-button>
+        <my-button
+          type="submit"
+          title="Добавить"
+          bgColor="#4679EC"
+          color="#FFFFFF"
+        ></my-button>
+      </div>
+    </form>
+    <v-notification
+      :isShow="notification.show"
+      :is_success="notification.is_success"
+      :header="notification.header"
+      :content="notification.content"
+      :btnFirst="notification.btnFirst"
+      :btnSecond="notification.btnSecond"
+      @handlerOne="handlerOne"
+      @handlerTwo="handlerTwo"
+    ></v-notification>
+    <v-notification
+      header="Ошибка"
+      :is_success="false"
+      :isShow="notificationError.show"
+      :content="notificationError.content"
+      :btnFirst="notificationError.btnFirst"
+      :btnSecond="notificationError.btnSecond"
+      @handlerOne="handlerOneError"
+      @handlerTwo="handlerTwoError"
+    ></v-notification>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["id"],
+  data() {
+    return {
+      notification: {
+        show: false,
+        is_success: true,
+        header: "",
+        content: "",
+        btnFirst: "",
+        btnSecond: "",
+      },
+      notificationError: {
+        show: false,
+        content: "",
+        btnFirst: "",
+        btnSecond: "",
+      },
+      editID: null,
+      user: {
+        active: 1,
+        full_name: "",
+        phone_number: "",
+        role: "",
+        email: "",
+        username: "",
+        email_notifications: false,
+        site_notifications: false,
+      },
+      password: "",
+      confirm: "",
+      old_password: "",
+      pass_title: "Пароль",
+      confirm_title: "Потвердите пароль",
+    };
+  },
+  created() {
+    if (this.$route.meta.id) {
+      this.editID = this.$route.meta.id;
+    }
+    if (this.id) {
+      this.editID = this.id;
+    }
+    if (this.editID) {
+      this.$store
+        .dispatch("addUser/fetchUserById", this.editID)
+        .then((res) => res.data)
+        .then((user) => {
+          this.user = {
+            ...user,
+            site_notifications: user.site_notifications === "active",
+            email_notifications: user.email_notifications === "active",
+          };
+          this.pass_title = "Новый пароль";
+          this.confirm_title = "Потвердите новый пароль";
+        })
+        .catch((err) => console.log(err));
+    }
+  },
+  methods: {
+    handlerOne() {
+      if (!this.editID) {
+        this.reset();
+      }
+      this.notification.show = false;
+    },
+    handlerTwo() {
+      this.reset();
+      if (this.id || !this.$route.meta.id) {
+        this.$router.push("/admins");
+      } else {
+        this.$router.push("/setting");
+      }
+    },
+    submitHandler() {
+      this.$validator.validateAll().then((isValid) => {
+        if (isValid) {
+          if (this.$route.path === "/add-admin" && this.password) {
+            this.$store
+              .dispatch("addUser/addItem", {
+                ...this.user,
+                password: this.password,
+                email_notifications: this.user.email_notifications
+                  ? "active"
+                  : "inactive",
+                site_notifications: this.user.site_notifications
+                  ? "active"
+                  : "inactive",
+              })
+              .then((res) => {
+                this.notification = {
+                  show: true,
+                  is_success: true,
+                  header: "Пользователь был добавлен успешно",
+                  content:
+                    "Теперь вы можете увидеть изменения в списке админов",
+                  btnFirst: "Вернуться",
+                  btnSecond: "Список админов",
+                };
+              })
+              .catch((err) => {
+                this.notification = {
+                  show: true,
+                  is_success: false,
+                  header: "Error",
+                  content: `${err.response.data.detail}`,
+                  btnFirst: "Вернуться",
+                  btnSecond: "Список админов",
+                };
+              });
+          } else if (this.editID) {
+            this.$store
+              .dispatch("addUser/updateItem", {
+                ...this.user,
+                password: this.password,
+                old_password: this.old_password,
+                email_notifications: this.user.email_notifications
+                  ? "active"
+                  : "inactive",
+                site_notifications: this.user.site_notifications
+                  ? "active"
+                  : "inactive",
+              })
+              .then((res) => {
+                if (this.id) {
+                  this.notification = {
+                    show: true,
+                    is_success: true,
+                    header: "Пользователь был изменён успешно",
+                    content:
+                      "Теперь вы можете увидеть изменения в списке админов",
+                    btnFirst: "Вернуться",
+                    btnSecond: "Список админов",
+                  };
+                } else {
+                  this.notification = {
+                    show: true,
+                    is_success: true,
+                    header: "Данные были изменены успешно",
+                    content: "Теперь вы можете увидеть изменения в настройках",
+                    btnFirst: "Вернуться",
+                    btnSecond: "Настройки",
+                  };
+                }
+              })
+              .catch((err) => {
+                this.notification = {
+                  show: true,
+                  is_success: false,
+                  header: "Error",
+                  content: `${err.response.data.detail}`,
+                  btnFirst: "Вернуться",
+                  btnSecond: this.id ? "Список админов" : "Настройки",
+                };
+              });
+          }
+        }
+      });
+    },
+    reset() {
+      this.$validator.reset();
+      this.notification = {
+        show: false,
+        is_success: true,
+        header: "",
+        content: "",
+        btnFirst: "",
+        btnSecond: "",
+      };
+      this.editID = null;
+      this.user = {
+        full_name: "",
+        phone_number: "",
+        role: "",
+        email: "",
+        username: "",
+        email_notifications: false,
+        site_notifications: true,
+      };
+      this.password = "";
+      this.confirm = "";
+      this.old_password = "";
+      this.pass_title = "Пароль";
+      this.confirm_title = "Потвердите пароль";
+    },
+  },
+
+  destroyed() {
+    this.reset();
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
